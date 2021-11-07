79db734ac96afd9f277066139387e98f9bc02e8d
udemy prueba <youremail@domain.com>
2021-11-07 03:11:16 +0000

    add test modification wms

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,5 +1,6 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String message="default message cambio test devops";
+		Assert.assertNotNull(message);
 	}
 
 }
